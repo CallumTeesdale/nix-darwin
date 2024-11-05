2a51a2f15e58b6b77d8e4d11756ee331287580b1
callum <callumjamesteesdale@gmail.com>
2024-11-05 08:13:27 +0000

    shell

--- home/shell.nix
+++ home/shell.nix
@@ -4,17 +4,12 @@
     enableCompletion = true;
     initExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-      source #{HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+      source ${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
       eval "$(atuin init zsh)"
       eval "$(fnm env --use-on-cd --shell zsh)"
-      
-      # .NET suggestions
-      _dotnet_zsh_complete() 
-      {
-        local completions=("$(dotnet complete "$words")")
-        reply=( "${(ps:\n:)completions}" )
-      }
-      compctl -K _dotnet_zsh_complete dotnet
+
+      # Load .NET CLI tools
+      export PATH="$PATH:$HOME/.dotnet/tools"
     '';
   };
 
