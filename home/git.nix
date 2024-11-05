2dfee1d4d4834bc9ea6c88299cb91492dca5aede
callum <callumjamesteesdale@gmail.com>
2024-11-05 08:37:55 +0000

    Update git configuration: fix typo, add SSH settings, and enable commit signing

--- home/git.nix
+++ home/git.nix
@@ -17,7 +17,7 @@
 
     includes = [
       {
-        # use diffrent email & name for work
+        # use different email & name for work
         path = "~/work/.gitconfig";
         condition = "gitdir:~/work/";
       }
@@ -27,6 +27,18 @@
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
+
+      # Use SSH format for git operations
+      url = {
+        "ssh://git@github.com/".insteadOf = "https://github.com/";
+        "ssh://git@gitlab.com/".insteadOf = "https://gitlab.com/";
+      };
+
+      # SSH signing configuration
+      gpg.format = "ssh";
+      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      commit.gpgsign = true;
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAzMUhK5XCP7PwKfJAYlmyPGYKrffz4gw0kwlEbyHxUM"; # This will be your 1Password SSH key
     };
 
     delta = {
@@ -51,6 +63,15 @@
       # aliases for submodule
       update = "submodule update --init --recursive";
       foreach = "submodule foreach";
+      
+      # verification aliases
+      verify = "verify-commit"; # Verify a signed commit
     };
   };
+
+  # Add SSH configuration for 1Password
+  home.file.".ssh/config".text = ''
+    Host *
+      IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+  '';
 }
